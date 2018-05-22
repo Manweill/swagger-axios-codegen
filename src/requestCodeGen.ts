26dde54d28fa7b2ef5a3fdbf1bf1aea40bae7b77
Manweill <mic.liangwenwei@foxmail.com>
2018-05-22 14:03:35 +0800

    给请求的参数params设置默认值{}

--- src/requestCodeGen.ts
+++ src/requestCodeGen.ts
@@ -3,14 +3,14 @@ import { toBaseType, refClassName, getMethodName } from './utils'
 import { IParameter, IPaths, ISwaggerOptions } from './baseInterfaces'
 
 declare class FormData {
-  append: Function
+  append: Function;
 }
 
 export interface IRequestMethods {
-  [key: string]: string
+  [key: string]: string;
 }
 export interface IRequestMethodInput {
-  [key: string]: string[]
+  [key: string]: string[];
 }
 const methodNameChange = { get: 'GET', post: 'POST', put: 'PUT', delete: 'DELETE' }
 /**
@@ -48,14 +48,11 @@ function getRequestParameters(params: IParameter[]) {
         data.append('${paramName}',params['${paramName}'],'${paramName}')
       }\n
       `
-    }
-    else if (p.in === 'path') {
+    } else if (p.in === 'path') {
       requestPathReplace += `url = url.replace('{${paramName}}',params['${paramName}']+'')\n`
-    }
-    else if (p.in === 'query') {
+    } else if (p.in === 'query') {
       queryParameters.push(`'${paramName}':params['${paramName}']`)
-    }
-    else if (p.in === 'body') {
+    } else if (p.in === 'body') {
       var body = p.schema ? `...params['${paramName}']` : `'${paramName}':params['${paramName}']`
       bodyParameters.push(body)
     }
@@ -70,7 +67,8 @@ export function requestCodeGen(paths: IPaths, options: ISwaggerOptions): string 
     let methodName = getMethodName(path)
     for (const [method, v] of Object.entries(request)) {
       methodName = options.methodNameMode === 'operationId' ? v.operationId : methodName
-      const contentType = v.consumes && v.consumes.includes('multipart/form-data') ? 'multipart/form-data' : 'application/json'
+      const contentType =
+        v.consumes && v.consumes.includes('multipart/form-data') ? 'multipart/form-data' : 'application/json'
       let formData = ''
       let pathReplace = ''
       // 获取类名
@@ -96,17 +94,15 @@ export function requestCodeGen(paths: IPaths, options: ISwaggerOptions): string 
           ${parsedParameters.requestParameters}
         }`)
 
-        parameters = `params: ${methodParamsName},`
+        parameters = `params: ${methodParamsName}={},`
         formData = parsedParameters.requestFormData ? 'data = new FormData();\n' + parsedParameters.requestFormData : ''
-        pathReplace = parsedParameters.requestPathReplace;
-
+        pathReplace = parsedParameters.requestPathReplace
       }
 
       let responseType =
-        v.responses['200'] &&
-          v.responses['200'].schema &&
-          v.responses['200'].schema.$ref ?
-          refClassName(v.responses['200'].schema.$ref) : 'any'
+        v.responses['200'] && v.responses['200'].schema && v.responses['200'].schema.$ref
+          ? refClassName(v.responses['200'].schema.$ref)
+          : 'any'
 
       // 模版
       RequestMethods[className] += `
@@ -115,7 +111,9 @@ export function requestCodeGen(paths: IPaths, options: ISwaggerOptions): string 
          * @param {IRequestOptions} [options] Override http request option.
          * @throws {RequiredError}
          */
-      ${options.useStaticMethod ? 'static' : ''} ${camelcase(methodName)}(${parameters}options:IRequestOptions={}):AxiosPromise<${responseType}> {
+      ${options.useStaticMethod ? 'static' : ''} ${camelcase(
+        methodName
+      )}(${parameters}options:IRequestOptions={}):AxiosPromise<${responseType}> {
 
         const configs = <AxiosRequestConfig>{ ...options,method:'${method}' };
         configs.headers = {
@@ -127,10 +125,18 @@ export function requestCodeGen(paths: IPaths, options: ISwaggerOptions): string 
         ${pathReplace}
         configs.url = url;
 
-        ${parsedParameters && parsedParameters.queryParameters.length > 0 ? "configs.params = {" + parsedParameters.queryParameters.join(',') + "}" : ''};
+        ${
+          parsedParameters && parsedParameters.queryParameters.length > 0
+            ? 'configs.params = {' + parsedParameters.queryParameters.join(',') + '}'
+            : ''
+        };
 
         let data = null;
-        ${parsedParameters && parsedParameters.bodyParameters.length > 0 ? "data = {" + parsedParameters.bodyParameters.join(',') + "}" : ''};
+        ${
+          parsedParameters && parsedParameters.bodyParameters.length > 0
+            ? 'data = {' + parsedParameters.bodyParameters.join(',') + '}'
+            : ''
+        };
 
         ${contentType === 'multipart/form-data' ? formData : ''}
 
@@ -154,7 +160,6 @@ export function requestCodeGen(paths: IPaths, options: ISwaggerOptions): string 
     item.forEach(text => {
       RequestClasses += text
     })
-
   })
   return RequestClasses
 }
