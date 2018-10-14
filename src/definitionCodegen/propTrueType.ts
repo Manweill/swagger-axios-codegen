ffc4f5c1f04fc0475dc5a71e42e6facb13e450c9
Manweill <mic.liangwenwei@foxmail.com>
2018-10-14 19:07:04 +0800

    优化propTureType.ts

--- src/definitionCodegen/propTrueType.ts
+++ src/definitionCodegen/propTrueType.ts
@@ -4,49 +4,47 @@ import { IDefinitionProperty } from "../baseInterfaces";
 export function propTrueType(v: IDefinitionProperty): {
   propType: string, isEnum: boolean, isArray: boolean, ref: string
 } {
-  let propType = ''
-  let isEnum = false
-  let isArray = false
-  let ref = ''
+  let result = {
+    propType: '',
+    isEnum: false,
+    isArray: false,
+    ref: ''
+  }
   if (v.$ref) {
     // 是引用类型
-    propType = refClassName(v.$ref)
-    ref = propType
+    result.propType = refClassName(v.$ref)
+    result.ref = result.propType
   }
   //是个数组
   else if (v.items) {
-    isArray = true
+    result.isArray = true
     if (v.items.$ref) {
       // 是个引用类型
-      ref = refClassName(v.items.$ref)
-      propType = ref + '[]'
+      result. = refClassName(v.items.$ref)
+      result.propType = result.ref + '[]'
     } else {
       if (v.items.type === "array") {
-        const result = propTrueType(v.items)
-        propType = result.propType
-        isEnum = result.isEnum
-        ref = result.ref
+        const currentResult = propTrueType(v.items)
+        result = { ...result, ...currentResult }
       } else if (!!v.items.enum) {
-        const result = propTrueType(v.items)
-        propType = result.propType
-        isEnum = result.isEnum
-        ref = result.ref
+        const currentResult = propTrueType(v.items)
+        result = { ...result, ...currentResult }
       } else {
-        propType = toBaseType(v.items.type) + '[]'
+        result.propType = toBaseType(v.items.type) + '[]'
       }
     }
   }
   // 是枚举 并且是字符串类型
   else if (v.enum && v.type === 'string') {
-    isEnum = true
-    propType = v.enum.map(item => `'${item}'='${item}'`).join(',')
+    result.isEnum = true
+    result.propType = v.enum.map(item => `'${item}'='${item}'`).join(',')
   }
   else if (v.enum) {
-    propType = v.type === 'string' ? v.enum.map(item => `'${item}'`).join('|') : v.enum.join('|')
+    result.propType = v.type === 'string' ? v.enum.map(item => `'${item}'`).join('|') : v.enum.join('|')
   }
   // 基本类型
   else {
-    propType = toBaseType(v.type)
+    result.propType = toBaseType(v.type)
   }
-  return { propType, isEnum, isArray, ref }
+  return result
 }
