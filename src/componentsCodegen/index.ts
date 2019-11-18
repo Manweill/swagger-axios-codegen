ecf3029e0b1ef23701e627bce93a25f62f1ca1f3
Manweill <mic.liangwenwei@foxmail.com>
2019-11-18 13:02:25 +0800

    fix #59 empty components in openApi v3.0

--- src/componentsCodegen/index.ts
+++ src/componentsCodegen/index.ts
@@ -1,29 +1,30 @@
 import { IDefinitions, IComponents } from '../swaggerInterfaces'
 import { refClassName } from '../utils'
-import { createDefinitionClass } from './createDefinitionClass';
-import { createDefinitionEnum } from './createDefinitionEnum';
-import { IDefinitionClasses, IDefinitionEnums } from '../baseInterfaces';
-
+import { createDefinitionClass } from './createDefinitionClass'
+import { createDefinitionEnum } from './createDefinitionEnum'
+import { IDefinitionClasses, IDefinitionEnums } from '../baseInterfaces'
 
 export function componentsCodegen(definitions: IComponents) {
   let definitionModels: IDefinitionClasses = {}
   let definitionEnums: IDefinitionEnums = {}
+  if (!definitions || !definitions.schemas) {
+    definitions = {
+      schemas: {}
+    }
+  }
   for (const [k, v] of Object.entries(definitions.schemas)) {
-
     let className = refClassName(k)
     let result = null
-    // is an enum definition,just in swagger openAPI v2 
+    // is an enum definition,just in swagger openAPI v2
     if (v.enum) {
       const enumDef = createDefinitionEnum(className, v.enum, v.type)
       definitionEnums[`#/components/schemas/${k}`] = {
         name: enumDef.name,
-        value: enumDef,
+        value: enumDef
       }
-    }
-    else if (v.type === 'array') {
+    } else if (v.type === 'array') {
       // #TODO
-    }
-    else {
+    } else {
       // default definition generate
       const { enums, model } = createDefinitionClass(className, v.properties)
       // console.log('createDefinitionClass', enums)
