bde1e628cd74a2bb083ec867baba81fd1211f351
Manweill <mic.liangwenwei@foxmail.com>
2021-02-24 13:51:57 +0800

    注释修改

--- src/V2/definitions/getEnumDefSpec.ts
+++ src/V2/definitions/getEnumDefSpec.ts
@@ -1,19 +1,21 @@
 import { IDictionary, IEnumDefSpec } from "../types/CodegenInterfaces"
 
+type IEnumArray = 'int' | 'string' | 'any'
+
 /**
  * 获取枚举类定义
  * @param className class名称
  * @param enum 枚举列表
- * @param type 枚举的类型
+ * @param type 枚举的值的类型
  */
-export function getEnumDefSpec(className: string, enumArray: any[], type: string): IEnumDefSpec {
+export function getEnumDefSpec(className: string, enumArray: IEnumArray[], type: string): IEnumDefSpec {
   let enumProps: IDictionary<string, any>[] = []
   if (type === 'string') {
     enumProps = enumArray.map(item => ({ [item]: item }))
   }
   else {
     enumProps = enumArray.map(item => {
-      const obj = isNaN(item) ? { [item]: item } : { ['key' + item]: item }
+      const obj = isNaN((item as any)) ? { [item]: item } : { ['key' + item]: item }
       return obj
     })
   }
