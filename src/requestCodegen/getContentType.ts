39a0fa7be79e3e98ae8f4b460eabc6ae2b3a5f9f
fairking <fairking@localhost>
2023-11-08 19:56:25 +0100

    Add support to x-www-form-urlencoded content type

--- src/requestCodegen/getContentType.ts
+++ src/requestCodegen/getContentType.ts
@@ -2,7 +2,13 @@ import { IRequestMethod } from '../swaggerInterfaces'
 
 export function getContentType(reqProps: IRequestMethod, isV3: boolean) {
   if (isV3) {
-    return reqProps?.requestBody?.content['multipart/form-data'] ? 'multipart/form-data' : 'application/json'
+    if (reqProps?.requestBody?.content['multipart/form-data']) {
+      return 'multipart/form-data'
+    } else if (reqProps?.requestBody?.content['application/x-www-form-urlencoded']) {
+      return 'application/x-www-form-urlencoded'
+    } else {
+      return 'application/json'
+    }
   }
 
   return reqProps.consumes && reqProps.consumes.includes('multipart/form-data')
