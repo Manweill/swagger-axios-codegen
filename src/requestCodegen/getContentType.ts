8d8696676df6cce4eeb056f0c95a1df134494c94
fairking <fairking@localhost>
2023-11-09 12:31:43 +0100

    Prefer Json over other content types

--- src/requestCodegen/getContentType.ts
+++ src/requestCodegen/getContentType.ts
@@ -2,16 +2,28 @@ import { IRequestMethod } from '../swaggerInterfaces'
 
 export function getContentType(reqProps: IRequestMethod, isV3: boolean) {
   if (isV3) {
-    if (reqProps?.requestBody?.content['multipart/form-data']) {
-      return 'multipart/form-data'
-    } else if (reqProps?.requestBody?.content['application/x-www-form-urlencoded']) {
-      return 'application/x-www-form-urlencoded'
-    } else {
+    // Prefer Json over other content types
+    if (reqProps?.requestBody?.content['application/json']) {
       return 'application/json'
     }
+    if (reqProps?.requestBody?.content['multipart/form-data']) {
+      return 'multipart/form-data'
+    }
+    if (reqProps?.requestBody?.content['application/x-www-form-urlencoded']) {
+      return 'application/x-www-form-urlencoded'
+    }
+    return 'application/json'
+  } else {
+    // Prefer Json over other content types
+    if (reqProps?.consumes?.includes('application/json')) {
+      return 'application/json'
+    }
+    if (reqProps?.consumes?.includes('multipart/form-data')) {
+      return 'multipart/form-data'
+    }
+    if (reqProps?.consumes?.includes('application/x-www-form-urlencoded')) {
+      return 'application/x-www-form-urlencoded'
+    }
+    return 'application/json'
   }
-
-  return reqProps.consumes && reqProps.consumes.includes('multipart/form-data')
-    ? 'multipart/form-data'
-    : 'application/json'
 }
