519dd750f8d33dd8bfb88c2916dad4a831db242a
fairking <fairking@localhost>
2023-11-13 21:50:11 +0100

    Prefer_Json_over_other_content_types (get content type based on schema instead)

--- src/requestCodegen/getContentType.ts
+++ src/requestCodegen/getContentType.ts
@@ -2,27 +2,16 @@ import { IRequestMethod } from '../swaggerInterfaces'
 
 export function getContentType(reqProps: IRequestMethod, isV3: boolean) {
   if (isV3) {
-    // Prefer Json over other content types
-    if (reqProps?.requestBody?.content['application/json']) {
-      return 'application/json'
-    }
-    if (reqProps?.requestBody?.content['multipart/form-data']) {
-      return 'multipart/form-data'
-    }
-    if (reqProps?.requestBody?.content['application/x-www-form-urlencoded']) {
-      return 'application/x-www-form-urlencoded'
+    if (reqProps?.requestBody?.content) {
+      const ct_keys = Object.keys(reqProps?.requestBody?.content)
+      if (ct_keys.length > 0) {
+        return ct_keys[0]
+      }
     }
     return 'application/json'
   } else {
-    // Prefer Json over other content types
-    if (reqProps?.consumes?.includes('application/json')) {
-      return 'application/json'
-    }
-    if (reqProps?.consumes?.includes('multipart/form-data')) {
-      return 'multipart/form-data'
-    }
-    if (reqProps?.consumes?.includes('application/x-www-form-urlencoded')) {
-      return 'application/x-www-form-urlencoded'
+    if (reqProps?.consumes && reqProps?.consumes.length > 0) {
+      return reqProps?.consumes[0]
     }
     return 'application/json'
   }
