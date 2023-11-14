2ca995874ce99fdd4994fdf48a56ebf702fbcc87
fairking <fairking@localhost>
2023-11-14 16:48:58 +0100

    Prefer_Json_over_other_content_types (prefer 'form-data' over json)

--- src/requestCodegen/getContentType.ts
+++ src/requestCodegen/getContentType.ts
@@ -5,13 +5,13 @@ export function getContentType(reqProps: IRequestMethod, isV3: boolean) {
     if (reqProps?.requestBody?.content) {
       const ct_keys = Object.keys(reqProps?.requestBody?.content)
       if (ct_keys.length > 0) {
-        return ct_keys[0]
+        return ct_keys.includes('multipart/form-data') ? 'multipart/form-data' : ct_keys[0]
       }
     }
     return 'application/json'
   } else {
     if (reqProps?.consumes && reqProps?.consumes.length > 0) {
-      return reqProps?.consumes[0]
+      return reqProps.consumes.includes('multipart/form-data') ? 'multipart/form-data' : reqProps.consumes[0]
     }
     return 'application/json'
   }
