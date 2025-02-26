d6e83e1d5127c648100768cdaf6753b084d5c027
Maxx Leung <manweill@outlook.com>
2025-02-26 11:18:08 +0800

    fix interface decription undefined swagger v2

--- src/definitionCodegen/index.ts
+++ src/definitionCodegen/index.ts
@@ -27,7 +27,7 @@ export function definitionsCodeGen(definitions: IDefinitions) {
         // #TODO
       } else {
         // default definition generate
-        const { enums, model } = createDefinitionClass(className, v.properties, v.required, v.description)
+        const { enums, model } = createDefinitionClass(className, v.properties, v.required, v.description ?? className)
         // console.log('createDefinitionClass', enums)
         enums.forEach(item => {
           // definitionModels[item.name] = {
